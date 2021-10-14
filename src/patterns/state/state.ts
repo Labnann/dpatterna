85b39c5c54190c9c240e00d0171f3da80cd4287c
Labnan <khalidmasum@iut-dhaka.edu>
2021-10-14 10:29:42 +0600

    Move state-patternCcode to Different File

---
+++ src/patterns/state/state.ts
@@ -0,0 +1,94 @@
+interface IDoorState {
+    onEnter(): void;
+
+    onPayOk(): void;
+
+    onPayFalse(): void;
+
+    message(): string;
+}
+
+class OpenState implements IDoorState {
+
+    door: Door;
+
+    constructor(door: Door) {
+        this.door = door;
+    }
+
+    message(): string {
+        return "open";
+    }
+
+    onEnter(): void {
+        this.door.setState(new ClosedState(this.door));
+    }
+
+    onPayOk(): void {
+        this.door.setState(new OpenState(this.door));
+    }
+
+    onPayFalse(): void {
+        this.door.setState(new OpenState(this.door));
+    }
+
+
+}
+
+class ClosedState implements IDoorState {
+    door: Door;
+
+    constructor(door: Door) {
+        this.door = door;
+    }
+
+    message(): string {
+        return "closed";
+    }
+
+    onEnter(): void {
+        this.door.setState(new ClosedState(this.door));
+    }
+
+    onPayOk(): void {
+        this.door.setState(new OpenState(this.door));
+    }
+
+    onPayFalse(): void {
+        this.door.setState(new ClosedState(this.door));
+    }
+
+}
+
+export class Door {
+
+    private _state: IDoorState;
+
+    constructor() {
+        this._state = new OpenState(this);
+
+    }
+
+
+    setState(value: IDoorState) {
+        this._state = value;
+    }
+
+
+    payFalse() {
+        this._state.onPayFalse();
+        return this._state.message();
+    }
+
+    payOk() {
+        this._state.onPayOk();
+        return this._state.message();
+    }
+
+    enter() {
+        this._state.onEnter();
+        return this._state.message();
+    }
+
+
+}
